983ff5294c0bdde7c2447ea9626bbf49158026ec
Joydeep Nath <joydeepnath279@gmail.com>
2026-04-08 00:39:08 +0530

    add next greater element

---
+++ String/150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "vector"
+using namespace std;
+
+int evalRPN(vector<string>& tokens)
+{
+
+
+
+
+
+  return 0;
+}
+
+int main() {
+
+  vector<string> tokens = {"2","1","+","3","*"};
+  evalRPN(tokens);
+  return 0;
+}
---
+++ String/next-greater-element.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+
+vector<int> nextGreater(vector<int> subArray,vector<int> mainArr) {
+
+  for (size_t i = 0; i < subArray.size(); i++) {
+    int j=0;
+
+    //STEP 1:
+    while (subArray[i]!=mainArr[j])
+      ++j;
+      ++j;
+
+    //STEP 2:
+    bool isGreaterFound=false;
+    while (mainArr.size()!=j) {
+        if (mainArr[j]>subArray[i]) {
+          isGreaterFound=true;
+          subArray[i]=mainArr[j];
+          break;
+        }
+        j++;
+    }
+    if (!isGreaterFound) {
+      subArray[i]=-1;
+    }
+
+
+
+  }
+
+  return subArray;
+
+}
+
+
+int main() {
+
+vector<int> subArray={1,4,2};
+vector<int> mainArr={1,5,4,2,8,7,};
+
+vector<int> result=nextGreater(subArray,mainArr);
+
+for (size_t i = 0; i < result.size(); i++) {
+  cout<<result[i]<<" ";
+}
+cout<<endl;
+  return 0;
+}
